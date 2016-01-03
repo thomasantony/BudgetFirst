dc42f960fb4ee4259c9c9c0e586ab3339af3256f
Thomas Antony <tantony@purdue.edu>
2016-01-02 20:00:36 -0500

    Photon boilerplate added

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,16 +1,184 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import styles from './Home.module.css';
+// import styles from './Home.module.css';
 
 
 export default class Home extends Component {
   render() {
+    // return (
+    //   <div>
+    //     <div classNameName={styles.container}>
+    //       <h2>Home</h2>
+    //       <Link to="/categories">Categories</Link><br/>
+    //       <Link to="/items">Items</Link><br/>
+    //     </div>
+    //   </div>
+    // );
     return (
-      <div>
-        <div className={styles.container}>
-          <h2>Home</h2>
-          <Link to="/categories">Categories</Link><br/>
-          <Link to="/items">Items</Link><br/>
+      <div className="window">
+
+        <header className="toolbar toolbar-header">
+          <h1 className="title">Photon</h1>
+        </header>
+        <div className="window-content">
+          <div className="pane-group">
+            <div className="pane pane-sm sidebar">
+              <nav className="nav-group">
+                <h5 className="nav-group-title">Favorites</h5>
+                <span className="nav-group-item">
+                  <span className="icon icon-home"></span>
+                  connors
+                </span>
+                <span className="nav-group-item active">
+                  <span className="icon icon-light-up"></span>
+                  Photon
+                </span>
+                <span className="nav-group-item">
+                  <span className="icon icon-download"></span>
+                  Downloads
+                </span>
+                <span className="nav-group-item">
+                  <span className="icon icon-folder"></span>
+                  Documents
+                </span>
+                <span className="nav-group-item">
+                  <span className="icon icon-window"></span>
+                  Applications
+                </span>
+                <span className="nav-group-item">
+                  <span className="icon icon-signal"></span>
+                  AirDrop
+                </span>
+                <span className="nav-group-item">
+                  <span className="icon icon-monitor"></span>
+                  Desktop
+                </span>
+              </nav>
+            </div>
+
+            <div className="pane">
+              <table className="table-striped">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Kind</th>
+                    <th>Date Modified</th>
+                    <th>Author</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>bars.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>base.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>button-groups.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>buttons.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>docs.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>forms.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>grid.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>icons.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>images.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>lists.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>mixins.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>navs.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>normalize.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>photon.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>tables.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>tabs.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>utilities.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                  <tr>
+                    <td>variables.scss</td>
+                    <td>Document</td>
+                    <td>Oct 13, 2015</td>
+                    <td>connors</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     );
