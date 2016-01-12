79560a6804124b1cb54dbc70b48a85432aab636d
Thomas Antony <tantony@purdue.edu>
2016-01-12 12:13:39 -0500

    Added account balance with coloring to account list

--- app/components/AccountList.js
+++ app/components/AccountList.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 
+// TODO: Make account list show actual balance rather than opening balance
+// TODO: Format account balance
 const AccountList = ({accounts}) => {
   let icons = {
     checking: 'icon-briefcase',
@@ -14,6 +16,9 @@ const AccountList = ({accounts}) => {
           <span className="nav-group-item" key={acct.id}>
             <span className={'icon '+icons[acct.type]}></span>
             {acct.name}
+            <span className='pull-right' style={{color: acct.openingBalance < 0? 'red':'inherit', fontWeight: 'bold'}}>
+              {acct.openingBalance}
+            </span>
           </span>
         )
       }
