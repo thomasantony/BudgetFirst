9ce2851bc17ec9b42279dc28a684b42be7fbef81
Thomas Antony <tantony@purdue.edu>
2016-01-12 14:55:50 -0500

    Added categories reducer to rootReducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import accounts from './accounts';
 import budget from './budget';
+import categories from './categories';
 import ledger from './ledger';
 
 const rootReducer = combineReducers({
   accounts,
   budget,
+  categories,
   ledger
 });
 
