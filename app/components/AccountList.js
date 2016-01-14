dc2eb5bea061d532f8cea15091d96f8798250f3e
Thomas Antony <tantony@purdue.edu>
2016-01-13 20:24:39 -0500

    Created AccountListItem component

--- app/components/AccountList.js
+++ app/components/AccountList.js
@@ -1,14 +1,9 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react';
 import { Pane, NavGroup, NavTitle, NavGroupItem } from 'react-photonkit';
-
+import AccountListItem from './AccountListItem';
 // TODO: Make account list show actual balance rather than opening balance
 // TODO: Format account balance
 const AccountList = ({accounts}) => {
-  let icons = {
-    checking: 'briefcase',
-    creditCard: 'credit-card',
-    autoLoan: 'rocket',  // why not? ^_^
-  }
   // <NavGroup activeKey={1}>
   //     <NavTitle>Accounts</NavTitle>
   //     {
@@ -22,15 +17,7 @@ const AccountList = ({accounts}) => {
     <nav className="nav-group">
       <h5 className="nav-group-title">Accounts</h5>
       {
-        accounts.map((acct) =>
-          <span className="nav-group-item active" key={acct.id}>
-            <span className={'icon icon-'+icons[acct.type]}></span>
-            {acct.name}
-            <span className='pull-right' style={{color: acct.openingBalance < 0? 'red':'inherit', fontWeight: 'bold'}}>
-              {acct.openingBalance}
-            </span>
-          </span>
-        )
+        accounts.map((acct) => <AccountListItem key={acct.id} account={acct}/> )
       }
     </nav>
   )
--- app/components/AccountListItem.js
+++ app/components/AccountListItem.js
@@ -1,6 +1,21 @@
-const AccountListItem = ({name, icon, balance}) => (
-
-)
+import React from 'react';
+const AccountListItem = ({account}) => {
+  let {name, type, openingBalance} = account;
+  let icons = {
+    checking: 'briefcase',
+    creditCard: 'credit-card',
+    autoLoan: 'rocket',  // why not? ^_^
+  }
+  return (
+    <span className="nav-group-item">
+      <span className={'icon icon-'+icons[type]}></span>
+      {name}
+      <span className='pull-right' style={{color: openingBalance < 0? 'red':'inherit', fontWeight: 'bold'}}>
+        {openingBalance}
+      </span>
+    </span>
+  )
+}
 
 
 export default AccountListItem;
